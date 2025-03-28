36f6dfe551063e4272528c153c4f6618e72043e3
peter <peterkinfe548@gmail.com>
2025-03-28 13:31:29 +0300

    setup server by requiring the app

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+const http = require('http');
+const port = 3000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
