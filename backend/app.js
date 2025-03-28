6411cb866f0e0373d5782663f23dc40d3ca15322
peter <peterkinfe548@gmail.com>
2025-03-28 23:17:17 +0300

    updated app middleware

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
