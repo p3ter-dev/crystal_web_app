a9f871152784d8a30cd6d6414ea3576a56f00650
peter <peterkinfe548@gmail.com>
2025-03-30 15:23:25 +0300

    changes on the app middleware

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
 const HttpError = require('./models/http-error');
 const userRoutes = require('./routes/userRoutes');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
