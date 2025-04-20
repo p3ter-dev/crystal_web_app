441bd3ef1c0b7e046880eaeb14f2fd5af2974360
peter <peterkinfe548@gmail.com>
2025-04-20 19:37:15 +0300

    added signup route for success page

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const authControllers = require('../controllers/authController');
+const { check } = require('express-validator');
 const router = express.Router();
 
-router.post('/signup', authControllers.signUpController);
+router.post('/signup', [
+    check('username').notEmpty().withMessage('username is required'),
+    check('email').isEmail().normalizeEmail().withMessage('please enter a valid email address'),
+    check('password').isLength({ min: 8 }).withMessage('password must be at least 8 characters long').trim(),
+], authControllers.signUpController);
 
 module.exports = router;
