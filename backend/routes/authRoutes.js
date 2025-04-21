3b066e96ea808fad352e937b0966e978d26e6e51
peter <peterkinfe548@gmail.com>
2025-04-21 21:33:07 +0300

    updated express validator to signup route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,9 +4,19 @@ const { check } = require('express-validator');
 const router = express.Router();
 
 router.post('/signup', [
-    check('username').notEmpty().withMessage('username is required'),
-    check('email').isEmail().normalizeEmail().withMessage('please enter a valid email address'),
-    check('password').isLength({ min: 8 }).withMessage('password must be at least 8 characters long').trim(),
+    check('username')
+        .notEmpty()
+        .withMessage('username is required'),
+    check('email')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('please enter a valid email address'),
+    check('password')
+        .isLength({ min: 8 })
+        .withMessage('password must be at least 8 characters long').trim(),
+    check('confirmPassword')
+        .custom((value, { req }) => value === req.body.password)
+        .withMessage('passwords do not match')
 ], authControllers.signUpController);
 
 module.exports = router;
