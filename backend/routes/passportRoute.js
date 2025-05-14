5abdba76b4bb8fe554da168bf6d4487837dc1d8a
peter <peterkinfe548@gmail.com>
2025-05-14 18:20:10 +0300

    added passport js route

---
+++ backend/routes/passportRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const passport = require('passport');
+require('../config/passportConfig');
+const router = express.Router();
+
+router.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/home');
+  }
+);
+
+module.exports = router;
