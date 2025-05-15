f69525c57a3938aac445bd84daeffd0660c6f3b6
peter <peterkinfe548@gmail.com>
2025-05-15 10:30:49 +0300

    updated passport js route

--- backend/routes/passportRoute.js
+++ backend/routes/passportRoute.js
@@ -10,6 +10,7 @@ router.get('/auth/google',
 router.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
+    req.session.user = req.user;
     res.redirect('/home');
   }
 );
