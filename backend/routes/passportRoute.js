e2d01830672cb3987574c1e134d3bd70108a3c4c
peter <peterkinfe548@gmail.com>
2025-05-17 20:52:46 +0300

    updated passport js route

--- backend/routes/passportRoute.js
+++ backend/routes/passportRoute.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const passport = require('passport');
-require('../config/passportConfig');
-const router = express.Router();
+// require('dotenv').config();
+// const express = require('express');
+// const passport = require('passport');
+// require('../config/passportConfig');
+// const router = express.Router();
 
-router.get('/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+// router.get('/auth/google',
+//     passport.authenticate('google', { scope: ['profile', 'email'] })
+// );
 
-router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => {
-    req.session.user = req.user;
-    res.redirect('/home');
-  }
-);
+// router.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   (req, res) => {
+//     req.session.user = req.user;
+//     res.redirect('/home');
+//   }
+// );
 
-module.exports = router;
+// module.exports = router;
