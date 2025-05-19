a598a5ba9d1e99fc193bb8e12cc3ef34faed573e
peter <peterkinfe548@gmail.com>
2025-05-19 10:46:04 +0300

    updated database connection

--- database/config/contactDb.js
+++ database/config/contactDb.js
@@ -2,6 +2,11 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+console.log(process.env.DATABASE_URL);
 
 module.exports = pool;
