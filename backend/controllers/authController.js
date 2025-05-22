fac30f326a480ecbb393e635877435ccb982ef18
peter <peterkinfe548@gmail.com>
2025-05-22 21:26:45 +0300

    updated authentication controller middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const pool = require('../../database/config/contactDb');
 const signUpController = async (req, res) => {
-const { username, email, password, confirmPassword } = req.body;
+    const { username, email, password, confirmPassword } = req.body;
 
     if (!username || !email || !password || !confirmPassword) {
         return res.status(400).render('errors/fields-required', { title: 'fields required' });
